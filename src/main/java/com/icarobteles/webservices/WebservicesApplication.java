0e3108ca8d759b2d1497416d6fdd7d01227746a7
Ícaro Teles <odevicaroteles@gmail.com>
2024-02-26 16:41:26 -0300

    feat: set time zone default with GMT

--- src/main/java/com/icarobteles/webservices/WebservicesApplication.java
+++ src/main/java/com/icarobteles/webservices/WebservicesApplication.java
@@ -1,13 +1,16 @@
 package com.icarobteles.webservices;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebservicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebservicesApplication.class, args);
-	}
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+    SpringApplication.run(WebservicesApplication.class, args);
+  }
 
 }
